7eadc9cde1b163d35d1cda780e1c9a3ba71f2723
enovu <sushanth26malipati@gmail.com>
2023-03-29 06:44:16 -0400

    added functionality of recording key strokes

---
+++ hecto/target/debug/deps/hecto-5d2decc1279fa542.d
@@ -0,0 +1,5 @@
+/Users/sushanthmalipati/hecto/hecto/hecto/target/debug/deps/hecto-5d2decc1279fa542: src/main.rs
+
+/Users/sushanthmalipati/hecto/hecto/hecto/target/debug/deps/hecto-5d2decc1279fa542.d: src/main.rs
+
+src/main.rs:
---
+++ hecto/target/debug/hecto.d
@@ -0,0 +1,1 @@
+/Users/sushanthmalipati/hecto/hecto/hecto/target/debug/hecto: /Users/sushanthmalipati/hecto/hecto/hecto/src/main.rs
